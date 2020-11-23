4ce222c2aad565ed5f04265cfb296ffc113dfa18
Lord-Gusarov <2367@holbertonschool.com>
2020-11-23 13:31:26 -0400

    Correctin typo in Fucntion decription

--- eatSpaces.c
+++ eatSpaces.c
@@ -1,7 +1,7 @@
 #include "shell.h"
 
 /**
- * eatSapces - finds and deletes spaces or tabs in the beggining of a string
+ * eatSpaces - finds and deletes spaces or tabs in the beggining of a string
  * @line: string to be analyzed for spaces
  * Return: new allocated memory with string and no spaces
  */
